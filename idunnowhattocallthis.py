88bbc30e7d67ff7712d67fef9f75216b9d35dfda
whizzer0 <whizzerZero@gmail.com>
2015-07-27 16:32:38 +0100

    Renamed main py, added logo

--- idunnowhattocallthis.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/example", methods=["GET"])
-def hello():
-    return render_template("example.html", data=None)
-
-if __name__ == "__main__":
-    app.run()
