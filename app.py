a895b099e71b3804d8c817b3ac7ce675aa837511
whizzer0 <whizzerZero@gmail.com>
2015-07-28 16:12:12 +0100

    WARN: Moved URL of start page from /example to root. Please tell/revert immediately if stuff is falling apart

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/example", methods=["GET"])
+@app.route("/", methods=["GET"])
 def example():
     return render_template("example.html", data=None)
 
