2eb79b2dc46d4533c23eb1281fcb187998ea153d
whizzer0 <whizzerZero@gmail.com>
2015-07-28 10:20:53 +0100

    Form page now works

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/example", methods=["GET"])
-def hello():
+def example():
     return render_template("example.html", data=None)
+
+@app.route("/form", methods=["GET"])
+def form():
+    return render_template("form.html", data=None)
 
 if __name__ == "__main__":
     app.run()
